e1bf9d47521511e05539c8f9ce7bcd652cc4293e
hbtoan2910 <hbtoan2910@gmail.com>
2020-02-21 16:29:37 -0500

    init commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Login from './src/views/landing/Login';
+import Registration from './src/views/landing/Registration';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
+//AppRegistry.registerComponent(appName, () => Registration);
